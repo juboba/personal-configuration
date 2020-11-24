a101abe750be799d654fd29e699f2e37fd4aef53
Julio Borja Barra <juboba@genial.ly>
2020-11-24 19:33:41 +0100

    Update lots of xmonad config (...)
    - Add "Sticky" windows
    - Enable Grid Layout
    - Add "launchVim" command
    - Update icons
    - Add "goFull" with manage to toggleStruts
    - Add shortcuts:
        - move through nonEmpty ws'
        - go to window
        - toggle notificacions
    - Add window layout for messaging
    - Update window rules
    - Update color theme

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -12,16 +12,22 @@ import XMonad.Actions.CycleWS
 -- import XMonad.Actions.CycleRecentWS
 -- import XMonad.Actions.FloatKeys
 import XMonad.Actions.UpdatePointer(updatePointer)
+-- import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.GridSelect
+-- import XMonad.Actions.SpawnOn
+
+import XMonad.Actions.CopyWindow
 
 import XMonad.Hooks.DynamicLog
--- import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.ManageDocks
 -- import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
 -- import XMonad.Hooks.SetWMName
 import XMonad.Hooks.UrgencyHook
 
--- import XMonad.Layout.PerWorkspace
+-- import XMonad.Layout.Renamed
+import XMonad.Layout.PerWorkspace
 -- import XMonad.Layout.ResizableTile
 -- import XMonad.Layout.StackTile
 -- import XMonad.Layout
@@ -30,7 +36,7 @@ import XMonad.Hooks.UrgencyHook
 -- import XMonad.Layout.Circle
 -- import XMonad.Layout.Grid
 -- import XMonad.Layout.IM
--- import XMonad.Layout.MosaicAlt
+import XMonad.Layout.MosaicAlt
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.Named
@@ -49,7 +55,7 @@ import XMonad.Layout.LayoutModifier(ModifiedLayout)
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 
--- import qualified Data.Map        as M
+import qualified Data.Map        as M
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
@@ -63,11 +69,25 @@ superKey = mod4Mask
 -- certain contrib modules.
 --
 myTerminal :: String
-myTerminal = "st"
+myTerminal = "terminal"
+
+-- My application launcher
+--
+myLauncher :: String
+myLauncher = "rofi -show-icons -show drun -modi drun,ssh -disable-history -sort -levenshtein-sort"
+
+-- showMe will spawn a little window with my face in it =)
+-- showMe :: X ()
+-- the ManageHook here is not working. Don't know why...
+-- showMe = spawnAndDo doIgnore "mplayer -vf mirror tv:// -tv device=/dev/video2:width=320:height=240"
+
+-- launchVim will spawn a vim window with the selectd file
+launchVim :: X ()
+launchVim = spawn "rofivim"
 
 -- Width of the window border in pixels.
 myBorderWidth :: Dimension
-myBorderWidth   = 1
+myBorderWidth   = 2
 
 -- The default number of workspaces (virtual screens) and their names.
 -- By default we use numeric strings, but any string may be used as a
@@ -79,15 +99,15 @@ myBorderWidth   = 1
 -- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
 --
 myWorkspaces :: [String]
-myWorkspaces    = [ "\61728" -- 
-                  , "\61508" -- 
-                  , "\62056" -- 
-                  , "\61888" -- 
-                  , "\61760" -- 
-                  , "\61477" -- ''
-                  , "\61574" -- 
-                  , "\61664" -- 
-                  , "\62057" -- 
+myWorkspaces    = [ " \61728 " -- 
+                  , " \61508 " -- 
+                  , " \62038 " -- 
+                  , " \62056 " -- 
+                  , " \61760 " -- 
+                  , " \61477 " -- ''
+                  , " \61574 " -- 
+                  , " \61664 " -- 
+                  , " \62057 " -- 
                   ]
 
 -- Border colors for unfocused and focused windows, respectively.
@@ -95,7 +115,12 @@ myNormalBorderColor :: String
 myNormalBorderColor  = "#000000"
 
 myFocusedBorderColor :: String
-myFocusedBorderColor = "#17729d"
+myFocusedBorderColor = "#e76c5e"
+
+goFull :: X ()
+goFull = do
+    sendMessage $ Toggle FULL
+    sendMessage ToggleStruts
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
@@ -104,25 +129,25 @@ myFocusedBorderColor = "#17729d"
 -- myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 --
 
-goFull :: X ()
-goFull = do
-    sendMessage $ Toggle FULL
-    sendMessage ToggleStruts
-
-
 myKeys :: [((KeyMask, KeySym), X())]
 myKeys =
     -- Toggle Fullscreen
     [ ((superKey, xK_f), goFull)
+    , ((superKey, xK_Left), moveTo Prev NonEmptyWS)
+    , ((superKey, xK_Right), moveTo Next NonEmptyWS)
     , ((superKey .|. shiftMask, xK_f), sendMessage ToggleStruts )
     -- Go to previous workspace
     , ((superKey, xK_Tab), toggleWS)
+    , ((superKey, xK_g), goToSelected defaultGSConfig)
     -- Cycle tiling modes
     -- , ((superKey, xK_0), sendMessage NextLayout)
     -- Send current workspace to next screen
     , ((superKey .|. shiftMask, xK_o), swapNextScreen)
+    , ((superKey .|. shiftMask, xK_e), launchVim)
     -- Focus next screen
     , ((superKey, xK_o), nextScreen)
+    , ((superKey, xK_s ), windows copyToAll) -- @@ Make focused window always visible
+    , ((superKey .|. shiftMask, xK_s ),  killAllOtherCopies) -- @@ Toggle window state back
     -- Focus urgent window
     , ((superKey, xK_x), focusUrgent)
 
@@ -132,10 +157,14 @@ myKeys =
     , ((superKey .|. shiftMask, xK_z), spawn "slock")
     -- Copy Emoji
     , ((superKey .|. shiftMask, xK_i), spawn $ "rofiemoji")
+    -- Launch color picker
+    , ((superKey .|. shiftMask, xK_y), spawn $ "pick-colour-picker")
     -- Launch rofi
-    , ((superKey, xK_p), spawn "rofi -show drun -modi drun,ssh -disable-history -sort -levenshtein-sort")
+    , ((superKey, xK_p), spawn myLauncher)
     -- Toggle window transparency
     , ((superKey, xK_r), spawn "transset 0.8 -t")
+    -- Toggle notifications
+    , ((superKey .|. shiftMask, xK_n), spawn "notify-send \"DUNST_COMMAND_TOGGLE\"")
     -- Launch Terminal
     -- , ((modm,               xK_F1     ), spawn $ XMonad.terminal conf)
     ]
@@ -155,28 +184,39 @@ myKeys =
 -- To match on the WM_NAME, you can use 'title' in the same way that
 -- 'className' and 'resource' are used below.
 --
+
 myManageHook :: Query (Endo WindowSet)
 myManageHook = composeAll
     [ isFullscreen --> doFullFloat
-    , className =? "Emacs"                     --> (doShift $ myWorkspaces !! 1)
-    , className =? "Thunderbird"               --> (doShift $ myWorkspaces !! 7)
-    , className =? "Chromium"                  --> (doShift $ myWorkspaces !! 2)
-    , title =? "Spotify Premium"               --> (doShift $ myWorkspaces !! 5)
-    , className =? "TelegramDesktop"           --> (doShift $ myWorkspaces !! 6)
-    , className =? "Microsoft Teams - Preview" --> (doShift $ myWorkspaces !! 6)
-    , className =? "Firefox"                   --> (doShift $ myWorkspaces !! 8)
-    , className =? "XClock"                    --> doIgnore
-    , stringProperty "_NET_WM_STATE(ATOM)" =? "_NET_WM_STATE_SKIP_TASKBAR"                    --> doIgnore
-    -- , resource  =? "desktop_window"            --> doIgnore
-    -- , className =? "Exe"                       --> doFloat
-    -- , className =? "Gvim"                      --> viewShift "^ vim"
-    -- , className =? "Gimp"                      --> doShift "8 grphx" <+> doFloat
-    , title =? "Picture-in-Picture"            --> doFloat -- Firefox videos
-    , title =? "Media viewer"                  --> doFloat -- Telegram media
-    , title =? "Microsoft Teams Notification"  --> doFloat -- Teams notifications
-    , className =? "Gsimplecal"                --> doFloat -- Calendar window
-    , title     =? "Copying Files"             --> doFloat ]
+    , className =? "Emacs"                                                 --> (doShift $ myWorkspaces !! 1)
+    , className =? "Thunderbird"                                           --> (doShift $ myWorkspaces !! 7)
+    , appName =? "chromium-browser (dev-profile)"                          --> (doShift $ myWorkspaces !! 2)
+    , appName =? "chromium-browser"                                        --> (doShift $ myWorkspaces !! 3)
+    , title =? "meet.google.com is sharing your screen."                   --> (doShift $ myWorkspaces !! 7)
+    , className =? "Spotify"                                               --> (doShift $ myWorkspaces !! 5)
+    , className =? "TelegramDesktop"                                       --> (doShift $ myWorkspaces !! 6)
+    , className =? "Slack"                                                 --> (doShift $ myWorkspaces !! 6)
+    , className =? "Firefox"                                               --> (doShift $ myWorkspaces !! 8)
+    , stringProperty "_NET_WM_STATE(ATOM)" =? "_NET_WM_STATE_SKIP_TASKBAR" --> doIgnore
+    -- , resource  =? "desktop_window"                                     --> doIgnore
+    -- , className =? "Exe"                                                --> doFloat
+    -- , className =? "Gvim"                                               --> viewShift "^ vim"
+    -- , className =? "Gimp"                                               --> doShift "8 grphx" <+> doFloat
+    , title =? "Picture-in-Picture"                                        --> doFloat -- Firefox videos
+    , className =? "Screen"                                                --> doFloat -- Screen share (with Screen.so)
+    , title =? "Media viewer"                                              --> doFloat -- Telegram media
+    , className =? "Gsimplecal"                                            --> doFloat -- Calendar window
+    , className =? ".pick-colour-picker-wrapped"                           --> doFloat -- Calendar window
+    , title     =? "Copying Files"                                         --> doFloat ]
     -- where viewShift = doF . liftM2 (.) W.greedyView W.shift
+
+------------------------------------------------------------------------
+-- Startup:
+myStartupHook :: X ()
+myStartupHook = do
+  -- _ <- let count = show  wsContainingCopies
+  --  in spawn ("notify-send " ++ count)
+  return ()
 
 ------------------------------------------------------------------------
 -- Layouts:
@@ -189,37 +229,44 @@ mainLayout = smartSpacing 10 $ Tall nmaster delta ratio where
 -- tallLayout :: ModifiedLayout
 tallLayout       = named "\61659"     $ avoidStruts $ mainLayout -- 
 wideLayout       = named "\61785"     $ avoidStruts $ Mirror mainLayout -- 
+
+messagingLayout  = named "\61659" $ smartSpacing 10 $ Tall nmaster delta ratio where
+    nmaster = 1
+    delta   = 3/100
+    ratio   = 3/4
+
 -- singleLayout     = named "\61640"   $ avoidStruts $ noBorders Full -- 
 -- circleLayout     = named "\61713"   $ Circle -- 
 -- twoPaneLayout    = named "Two Pane" $ TwoPane (2/100) (1/2)
--- mosaicLayout     = named "Mosaic"   $ MosaicAlt M.empty
+mosaicLayout     = named "Mosaic"   $ MosaicAlt M.empty
 -- gridLayout       = named "Grid"     $ Grid
--- spiralLayout     = named "Spiral"   $ spiral (1 % 1)
+-- spiralLayout     = named "Spiral"   $ spiral (6/7) -- (1 % 1)
 
 -- myLayoutHook :: ModifiedLayout ??
-myLayoutHook = tallLayout
+myLayoutHook = onWorkspace (myWorkspaces !! 6) messagingLayout
+  tallLayout
   ||| wideLayout
   -- ||| singleLayout
   -- ||| twoPaneLayout
   -- ||| circleLayout
-  -- ||| mosaicLayout
+  ||| mosaicLayout
   -- ||| gridLayout
   -- ||| spiralLayout
 
 currentWsStyle :: String -> String
-currentWsStyle = xmobarColor "#ffc145" ""
-
-layoutIndicatorStyle :: String -> String
-layoutIndicatorStyle = wrap "" "" . xmobarColor "#006494" ""
+currentWsStyle = xmobarColor "#eab354" ""
 
 urgentWsIndicatorStyle :: String -> String
-urgentWsIndicatorStyle = xmobarColor "#red4" "" . xmobarStrip
+urgentWsIndicatorStyle = xmobarColor "#de3c4b" ""
 
 visibleWsStyle :: String -> String
 visibleWsStyle = xmobarColor "#ba8d33" "LightSkyBlue4"
 
+layoutIndicatorStyle :: String -> String
+layoutIndicatorStyle = wrap "" "" . xmobarColor "#5f787b" ""
+
 windowTitleStyle :: String -> String
-windowTitleStyle = xmobarColor "#de3c4b" "" . shorten 20
+windowTitleStyle = xmobarColor "#e7e4e3" "" . shorten 30 . xmobarStrip . wrap " " " "
 
 main :: IO()
 main = do
@@ -238,12 +285,16 @@ main = do
                         , ppTitle   = windowTitleStyle
                         , ppUrgent  = urgentWsIndicatorStyle
                         , ppVisible = visibleWsStyle
-                        , ppWsSep   = "  "
+                        , ppWsSep   = " "
                         }
                         >> updatePointer (0.5, 0.5) (0, 0)
-        , manageHook        = manageDocks <+> myManageHook <+> manageHook def
+        , manageHook        = manageDocks
+                              <+> insertPosition Master Newer
+                              <+> myManageHook
+                              <+> manageHook def
         , modMask           = superKey
         , normalBorderColor = myNormalBorderColor
+        , startupHook       = myStartupHook
         , terminal          = myTerminal
         , workspaces        = myWorkspaces
         } `additionalKeys` myKeys
