59a134ad1b3474f42c752e297f373787af062df0
Julio Borja Barra <juboba@genial.ly>
2021-04-30 14:39:45 +0200

    Add rofiemoji plugin

--- nixpkgs/xdg.nix
+++ nixpkgs/xdg.nix
@@ -29,29 +29,39 @@ with pkgs; {
       source = ./dotfiles/xdg-configs/ranger;
     };
 
+    "rofi/plugins/rofiemoji" = {
+      source = fetchFromGitHub {
+        name = "rofiemoji";
+        owner = "nkoehring";
+        repo = "rofiemoji";
+        rev = "ad61572830c9d3c00e30eec078d46dad3cfdb4a2";
+        sha256 = "16rhb2cs8cqwflkcyw5dr77alp5wik4bv1dg66m4hkgcplxv0dx0";
+      };
+    };
+
     "sxiv" = {
       recursive = true;
       source = ./dotfiles/xdg-configs/sxiv;
     };
 
     "tmux/plugins/prefix-highlight" = {
-      source = (fetchFromGitHub {
+      source = fetchFromGitHub {
         name = "tmux-prefix-highlight";
         owner = "tmux-plugins";
         repo = "tmux-prefix-highlight";
         rev = "15acc6172300bc2eb13c81718dc53da6ae69de4f";
         sha256 = "08rkflfnynxgv2s26b33l199h6xcqdfmlqbyqa1wkw7h85br3dgl";
-      });
+      };
     };
 
     "tmux/plugins/extrakto" = {
-      source = (fetchFromGitHub {
+      source = fetchFromGitHub {
         name = "extrakto";
         owner = "laktak";
         repo = "extrakto";
         rev = "de8ac3e8a9fa887382649784ed8cae81f5757f77";
         sha256 = "0mkp9r6mipdm7408w7ls1vfn6i3hj19nmir2bvfcp12b69zlzc47";
-      });
+      };
     };
 
     "tmux/themes" = {
