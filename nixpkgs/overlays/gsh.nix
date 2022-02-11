43c33f3e90d0e0f3d79d61eec14653c565ddb5f7
Julio Borja Barra <juboba@genial.ly>
2022-02-11 16:01:12 +0100

    Update gsh overlay

--- nixpkgs/overlays/gsh.nix
+++ nixpkgs/overlays/gsh.nix
@@ -1,8 +1,5 @@
 self: super: {
     gsh = import (fetchGit {
       url = "git@github.com:Genially/gsh";
-      #ref = "master";
-      #rev = "v0.3.0";
-      #sha256 = "10xhblbyw8mvak58d294hbxxnf5sq0akj6qldv7brgm6944zppm0";
-    });
+    }) {};
   }
