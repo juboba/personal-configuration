2ce6f472b077a46459110301b28006e2992814bf
Julio Borja Barra <juboba@genial.ly>
2022-06-09 20:29:53 +0200

    Update packages

--- nixpkgs/packages.nix
+++ nixpkgs/packages.nix
@@ -1,13 +1,21 @@
 { pkgs, ... }:
 
-let gitduck = (pkgs.callPackage ./gitduck {}).gitduck;
-    nodemon = (pkgs.callPackage ./nodemon {}).nodemon;
-    typescript-language-server = (pkgs.callPackage ./typescript-language-server {}).typescript-language-server;
-    yarnWithNode10 = pkgs.yarn.overrideAttrs (oldAttrs: rec {
+let 
+  nodejs-16_13_1 = (import (fetchTarball {
+      name = "nixpkgs-node16_13_1";
+      url = "https://github.com/NixOS/nixpkgs/archive/c82b46413401efa740a0b994f52e9903a4f6dcd5.tar.gz";
+      sha256 = "13s8g6p0gzpa1q6mwc2fj2v451dsars67m4mwciimgfwhdlxx0bk";
+    }) {}).nodejs-16_x;
+
+    yarnWithNode16 = pkgs.yarn.overrideAttrs (oldAttrs: rec {
       buildInputs = with pkgs; [
-        nodejs-10_x
+        nodejs-16_13_1
       ];
     });
+
+    nodemon = (pkgs.callPackage ./nodemon { inherit pkgs; }).nodemon;
+
+    typescript-language-server = (pkgs.callPackage ./typescript-language-server { inherit pkgs; }).typescript-language-server;
 
     comma = with pkgs; callPackage (fetchFromGitHub {
       name = "comma";
@@ -15,7 +23,7 @@ let gitduck = (pkgs.callPackage ./gitduck {}).gitduck;
       repo = "comma";
       rev = "02e3e5545b0c62595a77f3d5de1223c536af0614";
       sha256 = "0qgg632ky6bnwkf9kh1z4f12dilkmdy0xgpal26g2vv416di04jq";
-    }) {};
+    }) { inherit pkgs; };
 
 in with pkgs; [
   # Utils
@@ -40,6 +48,7 @@ in with pkgs; [
   ispell
   nixfmt
   procs
+  qsudo
   ripgrep
   simplescreenrecorder
   steam-run
@@ -87,24 +96,28 @@ in with pkgs; [
   tdesktop
 
   # Development
-  (emacsWithPackages (epkgs: [ epkgs.emacsql-sqlite ]))
-  #doom-emacs
-  gitduck
+  awscli2
+  cypress
+  docker-compose
+  gcc
   gotty
   gsh
   highlight
   hugo
   jq
+  kubectl
   ngrok
+  nodejs-16_13_1
+  yarnWithNode16
   nodePackages.node2nix
-  nodejs-10_x
   nodemon
   peek
   pick-colour-picker
   python3
   robo3t
   shellcheck
+  trayer
   typescript-language-server
-  yarnWithNode10
+  unzip
 ]
 
